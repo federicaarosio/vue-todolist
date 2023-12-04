6425cdca1bcd035b5efdc0dc611d42c6d0473767
federicaarosio <arosio.fede@gmail.com>
2023-12-04 15:13:59 +0100

    added array, v-model and v-for

--- js/script.js
+++ js/script.js
@@ -3,6 +3,33 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            newHikeElement: '',
+            hikes: [
+                {
+                    text: 'Rifugio Brioschi',
+                    done: false,
+                },
+                {
+                    text: 'Rifugio Rosalba',
+                    done: true,
+                },
+                {
+                    text: 'Lago del Barbellino',
+                    done: true,
+                },
+                {
+                    text: 'Vetta Arera',
+                    done: false,
+                },
+                {
+                    text: 'Rifugio Segantini',
+                    done: true,
+                },
+                {
+                    text: 'rifugio Dodici Apostoli',
+                    done: false,
+                },
+            ],
         }
     },
 }).mount('#app');
